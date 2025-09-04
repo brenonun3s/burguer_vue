40dfbb2a630d3c901179919ed850d29c97e61cde
Breno Nunes <morais.brenonunes@hotmail.com>
2025-09-04 16:50:27 -0300

    feat: development of the Banner, Footer, Navbar, Home and Orders components started

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,43 @@
 <template>
-  <Navbar />
-  <router-view/>
+  <Navbar :logo="logo_src" :alt="app_name" />
+  <router-view />
   <Footer />
 </template>
 <script>
-  import Navbar from './components/Navbar.vue';
-  import Footer from './components/Footer.vue';
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
-  export default {
-    components: {
-      Navbar,
-      Footer
-    }
-  }
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burguer",
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
+.main-container {
+  margin: 0px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
-
-
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="home">
-    <h1>HOME</h1>
-
+  <Banner />
+  <p>banner</p>
+  <div class="main-container">
+    <h1>Monte o seu burguer:</h1>
+    <p>Formulario</p>
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Banner,
+  },
+};
 </script>
